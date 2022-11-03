94796f68a952a93f2cc6ed1df75ca7cbb8b2d56e
Manav Misra <manavm@visionwebsoft.com>
2022-11-02 19:37:11 -0500

    fix: add actual fxns. to do the conversions

--- src/utils/tryConvert.js
+++ src/utils/tryConvert.js
@@ -1,9 +1,21 @@
-export function tryConvert(temperature, convert) {
+const conversions = {
+  convertToCelsius(fahrenheit) {
+    return ((fahrenheit - 32) * 5) / 9;
+  },
+  convertToFahrenheit(celsius) {
+    return (celsius * 9) / 5 + 32;
+  },
+};
+
+export function tryConvert(temperature, conversion) {
   const input = parseFloat(temperature);
   if (Number.isNaN(input)) {
     return "";
   }
-  const output = convert(input);
+
+  // 'conversion' will be either 'convertToCelsius' or 'convertToFahrenheit'
+  const output = conversions[conversion](input);
+
   const rounded = Math.round(output * 1000) / 1000;
   return rounded.toString();
 }
